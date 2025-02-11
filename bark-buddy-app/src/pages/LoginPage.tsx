b7f89213013182e2a11005d06dfa6888f05c25b4
Jack Whisler <78163023+jackwhisler1@users.noreply.github.com>
2025-02-11 17:38:46 -0600

    adds logo to login

--- bark-buddy-app/src/pages/LoginPage.tsx
+++ bark-buddy-app/src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "../components/Auth/Login";
 import Footer from "../components/Footer";
+import logo from "../assets/bark_buddy_logo.png";
 
 interface LoginPageProps {
   onLogin: () => void;
@@ -9,7 +10,12 @@ interface LoginPageProps {
 const LoginPage: React.FC<LoginPageProps> = ({ onLogin }) => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      <div className="flex-grow flex items-center justify-center p-6">
+      <div className="flex-grow flex flex-col items-center justify-center p-6">
+        <img
+          src={logo}
+          alt="Bark Buddy Logo"
+          className="h-40 w-75 mb-6 rounded"
+        />
         <Login onLogin={onLogin} />
       </div>
       <Footer />
